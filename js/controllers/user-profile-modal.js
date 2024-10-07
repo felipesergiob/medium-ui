7223ffe96017feb70aa5ab6f8ea13950783969ce
felipesergiob <felipessmbf@gmail.com>
2024-10-07 14:55:53 -0300

    Feat: update pass & refactor update profile form

--- js/controllers/user-profile-modal.js
+++ js/controllers/user-profile-modal.js
@@ -1,6 +1,6 @@
-myApp.controller('userProfileModalController', function($scope, UserService, $http, $rootScope , $modalInstance) {
+myApp.controller('userProfileModalController', function($scope, UserService, $http, $rootScope, $modalInstance) {
     $scope.userProfile = {};
-    $scope.showDeleteConfirmationModal = false;
+    let originalProfile = {};
 
     const init = () => {
         readUserProfile();
@@ -13,37 +13,93 @@ myApp.controller('userProfileModalController', function($scope, UserService, $ht
         UserService.read().then((response) => {
             if (response && response.data) {
                 $scope.userProfile = response.data.data;
+                originalProfile = angular.copy($scope.userProfile);
             } else {
                 $scope.userProfile.message = 'Resposta da API inválida';
             }
-        }).catch((error) => {
+        }).catch(() => {
             $scope.userProfile.message = 'Erro ao carregar perfil do usuário';
         });
     };
 
-    $scope.updateUserProfile = () => {        
+    $scope.isProfileChanged = () => {
+        return $scope.userProfile.name !== originalProfile.name ||
+               $scope.userProfile.email !== originalProfile.email;
+    };
+
+    $scope.isPasswordChanged = () => {
+        return $scope.userProfile.currentPassword && $scope.userProfile.newPassword;
+    };
+
+    $scope.updateUserProfile = () => {
         const token = localStorage.getItem('token');
         $http.defaults.headers.common.Authorization = 'Bearer ' + token;
 
-        UserService.update($scope.userProfile).then((response) => {
-            if (response && response.data) {
-                $scope.userProfile.message = 'Perfil atualizado com sucesso';
-                $rootScope.$broadcast('userProfileUpdated'); 
-                $modalInstance.close();
-            } else {
-                $scope.userProfile.message = 'Falha ao atualizar perfil';
-            }
-        }).catch((error) => {
-            $scope.userProfile.message = 'Erro ao atualizar perfil do usuário';
-        });
+        let profileUpdated = false;
+        let passwordUpdated = false;
+
+        let updatePromises = [];
+
+        if ($scope.isProfileChanged()) {
+            const profilePayload = {
+                name: $scope.userProfile.name,
+                email: $scope.userProfile.email
+            };
+            updatePromises.push(
+                UserService.update(profilePayload).then(() => {
+                    profileUpdated = true;
+                })
+            );
+        }
+
+        if ($scope.isPasswordChanged()) {
+            const passwordPayload = {
+                currentPassword: $scope.userProfile.currentPassword,
+                newPassword: $scope.userProfile.newPassword
+            };
+            updatePromises.push(
+                UserService.updatePassword(passwordPayload).then(() => {
+                    passwordUpdated = true;
+                })
+            );
+        }
+
+        Promise.all(updatePromises)
+            .then(() => {
+                if (profileUpdated || passwordUpdated) {
+                    $scope.userProfile.message = 'Perfil atualizado com sucesso';
+                    $rootScope.$broadcast('userProfileUpdated');
+                    resetPasswordFields(); 
+                    $modalInstance.close();
+                } else {
+                    $scope.userProfile.message = 'Nenhuma alteração detectada';
+                }
+            })
+            .catch(() => {
+                $scope.userProfile.message = 'Erro ao atualizar perfil do usuário';
+            });
+    };
+
+    const resetPasswordFields = () => {
+        $scope.userProfile.currentPassword = '';
+        $scope.userProfile.newPassword = '';
     };
 
     $scope.openDeleteConfirmationModal = () => {
-        $scope.showDeleteConfirmationModal = true;
-    };
-
-    $scope.closeDeleteConfirmationModal = () => {
-        $scope.showDeleteConfirmationModal = false;
+        Swal.fire({
+            title: 'Você tem certeza?',
+            text: "Esta ação não pode ser desfeita!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#e74c3c',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Sim, deletar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $scope.deleteUserProfile();
+            }
+        });
     };
 
     $scope.deleteUserProfile = () => {
@@ -53,11 +109,12 @@ myApp.controller('userProfileModalController', function($scope, UserService, $ht
         UserService.delete().then((response) => {
             if (response && response.data) {
                 $scope.userProfile.message = 'Conta deletada com sucesso';
-                $scope.closeDeleteConfirmationModal();
+                Swal.fire('Deletado!', 'Sua conta foi deletada.', 'success');
+                $modalInstance.close();
             } else {
                 $scope.userProfile.message = 'Falha ao deletar a conta';
             }
-        }).catch((error) => {
+        }).catch(() => {
             $scope.userProfile.message = 'Erro ao deletar a conta do usuário';
         });
     };
--- js/services/user.js
+++ js/services/user.js
@@ -2,14 +2,19 @@ myApp.service('UserService', function ($http) {
     this.login = (credentials) => {
         return $http.post(`${baseUrl}users/login`, credentials);
     };
+    
     this.read = () => {
         return $http.get(`${baseUrl}users/profile`);
     };
+    
     this.update = (user) => {
-        console.log('update', user);
-        
         return $http.put(`${baseUrl}users/update`, user);
     };
+
+    this.updatePassword = (passwords) => {
+        return $http.put(`${baseUrl}users/update/pass`, passwords);
+    };
+
     this.delete = () => {
         return $http.delete(`${baseUrl}users/delete`);
     };
